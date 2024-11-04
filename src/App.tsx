faf9ef2eb30a21069e8d8defcdc3113a4fddbf5d
yonghee Lee <yonghee1244@gmail.com>
2024-11-04 18:17:33 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,20 +16,22 @@ let data:Resturant = {
   menu:[{name:'rose pasta', price:2000, category:"pasta"},{name:'rose pasta', price:2000, category:"pasta"}]
 }
 
+let test:string = 'adgs';
+
 const App:React.FC = () => {
   const [myRestaurant, setMyRestaurant] = useState<Resturant>(data);
   const changeAddress = (address:Address) => {
     setMyRestaurant({...myRestaurant, address:address})
   }
 
-  const showBestMenuName = (name:string) => {
+  const showBestMenuName = (name:string):string => {
     return name;
   }
 
   return (
     <div className="App">
       <Store info={myRestaurant} changeAddress={changeAddress} />
-      <BestMenu name="불고기피자" category="피자" price={1000} />
+      <BestMenu name="불고기피자" category="피자" showBestMenuName={showBestMenuName} />
     </div>
   );
 }
